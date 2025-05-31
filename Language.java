84b238f1fdfcca79140328cf49ef3392dac80beb
ktykty0525 <160840080+ktykty0525@users.noreply.github.com>
2025-05-31 19:35:59 +0900

    Update Language.java

--- Language.java
+++ Language.java
@@ -1,18 +1,13 @@
 package basicWeb;
 
 public class Language {
-    public static void changeLanguage(KnuLectureCrawlerGUI gui, String lang) {
-        gui.setTitle(LanguageChange.getLabel(lang, "title"));
-        gui.searchBtn.setText(LanguageChange.getLabel(lang, "search"));
-        gui.saveBtn.setText(LanguageChange.getLabel(lang, "save"));
-        gui.statusLabel.setText(LanguageChange.getLabel(lang, "status"));
-
-        gui.yearLabel.setText(LanguageChange.getLabel(lang, "year"));
-        gui.semesterLabel.setText(LanguageChange.getLabel(lang, "semester"));
-        gui.subjectLabel.setText(LanguageChange.getLabel(lang, "subject"));
-
-        for (int i = 0; i < gui.tableModel.getColumnCount(); i++) {
-            gui.tableModel.setColumnIdentifiers(LanguageChange.getHeaders(lang));
-        }
+    public static void applyKoreanLabels(KnuLectureCrawlerGUI gui, int sequence) {
+        gui.setTitle("KNU 강의 계획서 크롤러");
+        gui.searchBtn.setText("검색");
+        gui.statusLabel.setText("대기 중...");
+        gui.yearLabel.setText("개설연도:");
+        gui.semesterLabel.setText("학기:");
+        gui.gradeLabel.setText("학년:");
+        gui.tableModel.setColumnIdentifiers(LanguageChange.getHeaders(sequence));
     }
 }
